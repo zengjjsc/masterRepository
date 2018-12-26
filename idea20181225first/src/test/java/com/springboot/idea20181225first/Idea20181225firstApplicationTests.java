0def514b49ecaceda33cadbfdf32f23a31d044c4
曾浚桔 <zengjunjie0@qq.com>
2018-12-26 18:26:33 +0800

    1

---
+++ idea20181225first/src/test/java/com/springboot/idea20181225first/Idea20181225firstApplicationTests.java
@@ -0,0 +1,17 @@
+package com.springboot.idea20181225first;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Idea20181225firstApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
